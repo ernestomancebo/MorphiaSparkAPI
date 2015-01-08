afc47e20c49d4974b531ef4a13400990ff1593c3
Ernesto Mancebo <ernesmancebo@gmail.com>
2015-01-08 13:17:01 -0400

    Added SparkApplicationInjector for DI

--- src/main/java/com/flectosystems/morphiasparkapi/test.java
+++ src/main/java/com/flectosystems/morphiasparkapi/test.java
@@ -1,24 +1,42 @@
 package com.flectosystems.morphiasparkapi;
 
+import java.lang.reflect.Method;
+
 public class test {
 
     public static void main(String... args) {
-SingleClass instance1 = new SingleClass();
-        SingleClass instance2 = SingleClass().;
+
+        // Just testing reflection
+        try {
+            Class applicationClass = Class.forName("com.flectosystems.morphiasparkapi.SingleClass");
+            Method method = applicationClass.getDeclaredMethod("getInstance", new Class[0]);
+            System.out.println("Method: " + method);
+            SingleClass s = (SingleClass) method.invoke(applicationClass, new Object[0]);
+            s.printInstance();
+
+            SingleClass s2 = (SingleClass) method.invoke(null, null);
+            s2.printInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
 
 class SingleClass {
-    SingleClass instance = null;
 
-    public SingleClass() {
-        System.out.print("Instance of SingleClass");
+    static SingleClass instance = new SingleClass();
+
+    private SingleClass() {
     }
 
-    public SingleClass getInstance() {
-        if (null == instance)
-            instance = this;
+    void printInstance() {
+        System.out.println("Instance of SingleClass " + this.toString());
+    }
+
+    public static SingleClass getInstance() {
+//        if (null == instance)
+//            instance = this;
         return instance;
     }
-
 }
